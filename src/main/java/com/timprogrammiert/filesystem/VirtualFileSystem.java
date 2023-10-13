4d7aa714bbc8d109278e6745b6ec6215691ef14c
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-13 21:48:46 +0200

    Small tests

--- src/main/java/com/timprogrammiert/filesystem/VirtualFileSystem.java
+++ src/main/java/com/timprogrammiert/filesystem/VirtualFileSystem.java
@@ -1,19 +1,29 @@
 package com.timprogrammiert.filesystem;
 
+import com.timprogrammiert.util.DirectoryInfo;
+
 /**
  * @author tmatz
  */
 public class VirtualFileSystem {
     String[] fileSystemStructure = new String[]{"etc", "var", "home"};
-    BaseFileSystemObject rootObject = new DirectoryObject("/");
+    BaseFileSystemObject rootObject;
+    BaseFileSystemObject testDirectory;
 
     public VirtualFileSystem() {
-
+        rootObject = new DirectoryObject("/");
+        testDirectory = new DirectoryObject("test", rootObject.getSpecificChildren("etc"));
+        createFileSystemStructure();
     }
 
     private void createFileSystemStructure(){
         for (String dirName: fileSystemStructure) {
-            rootObject.addNewDirectory(new DirectoryObject(dirName));
+            rootObject.addNewDirectory(new DirectoryObject(dirName, rootObject));
         }
+        rootObject.getSpecificChildren("etc").addNewDirectory(testDirectory);
+    }
+
+    public BaseFileSystemObject getRootFileSystem(){
+        return rootObject;
     }
 }
