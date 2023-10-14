a2fc2bd598d938fec01966628248c73ef980f14d
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-14 10:50:26 +0200

    - Updated to work with new Command Structure (Interface)

--- src/main/java/com/timprogrammiert/terminal/Terminal.java
+++ src/main/java/com/timprogrammiert/terminal/Terminal.java
@@ -1,5 +1,6 @@
 package com.timprogrammiert.terminal;
 
+import com.timprogrammiert.commands.CommandParser;
 import com.timprogrammiert.commands.LsCommand;
 import com.timprogrammiert.host.Host;
 import com.timprogrammiert.util.DirectoryInfo;
@@ -11,21 +12,23 @@ import java.util.Scanner;
  */
 public class Terminal {
     private Host host;
+    private CommandParser commandParser;
     public Terminal() {
         host = new Host();
+        commandParser = new CommandParser(host);
         run();
     }
 
     private void run(){
         Scanner scanner = new Scanner(System.in);
-        String promptText = DirectoryInfo.getAbsolutPath(host.getCurrentDirectory()) + " ";
         String input = null;
         while (true){
-            System.out.print(promptText);
+            System.out.print(DirectoryInfo.getAbsolutPath(host.getCurrentDirectory()) + " ");
             input = scanner.nextLine();
-            String[] testCommand = new String[]{"/etc"};
-            LsCommand.execute(testCommand, host);
-
+            //String[] testCommand = new String[]{"/etc"};
+            //LsCommand.execute(testCommand, host);
+            String[] commandInput = input.split(" ");
+            commandParser.parseCommand(commandInput);
             if(input.equals("exit")) return;
 
         }
