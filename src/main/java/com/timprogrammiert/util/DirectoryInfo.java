3781392aa09be0dd917a93a8e0430e95765ea71e
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-14 09:53:39 +0200

    - Fixed a bug where the Directory Path couldn be displayed correctly - Fixed a bug where the Directory got two "/" like //etc

--- src/main/java/com/timprogrammiert/util/DirectoryInfo.java
+++ src/main/java/com/timprogrammiert/util/DirectoryInfo.java
@@ -12,20 +12,31 @@ import java.util.List;
  */
 public class DirectoryInfo {
     public static String getAbsolutPath(BaseFileSystemObject fileSystemObject){
-        // Go to root
-        BaseFileSystemObject parent = fileSystemObject;
-        List<String> allParents = new ArrayList<>();
-        while (parent.getParent() != null){
-            parent = parent.getParent();
-            allParents.add(parent.getParent().getName());
-        }
-        List<String> correctOrder = new ArrayList<>(allParents);
-        Collections.reverse(correctOrder);
+            BaseFileSystemObject parent = fileSystemObject;
+            List<String> allParents = new ArrayList<>();
+            allParents.add(parent.getName());
 
-        StringBuilder output = new StringBuilder("/");
-        for (String dirName: correctOrder) {
-            output.append(dirName).append("/");
+            while (parent.getParent() != null) {
+                parent = parent.getParent();
+                allParents.add(parent.getName());
+            }
+
+            // Reverse to ensure absolute Path is in correct order
+            // dirNames get stored fromn child to Parent, we want from Parent to child
+            Collections.reverse(allParents);
+
+            StringBuilder output = new StringBuilder();
+            for (String dirName : allParents) {
+                output.append(dirName);
+                if(dirName.equals("/")) continue;
+                // Check if other Directorys still exists
+                if (!dirName.equals(allParents.get(allParents.size() - 1))) {
+                    output.append("/");
+                }
+            }
+
+            return output.toString();
         }
-        return output.toString();
-    }
+
 }
+
