c511e84200191fc8363e6a98759c51230da2ebff
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-15 14:43:23 +0200

    - Moved Functions from LsCommand directly to DirectoryInfo

--- src/main/java/com/timprogrammiert/util/DirectoryInfo.java
+++ src/main/java/com/timprogrammiert/util/DirectoryInfo.java
@@ -1,8 +1,13 @@
 package com.timprogrammiert.util;
 
+import com.timprogrammiert.exceptions.FileNotExistsException;
+import com.timprogrammiert.exceptions.NotADirectoryException;
 import com.timprogrammiert.filesystem.BaseFileSystemObject;
+import com.timprogrammiert.filesystem.DirectoryObject;
+import com.timprogrammiert.host.Host;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -10,7 +15,7 @@ import java.util.List;
  * @author tmatz
  */
 public class DirectoryInfo {
-    public static String getAbsolutPath(BaseFileSystemObject fileSystemObject){
+    public static String getAbsolutePathByFileSystem(BaseFileSystemObject fileSystemObject){
             BaseFileSystemObject parent = fileSystemObject;
             List<String> allParents = new ArrayList<>();
             allParents.add(parent.getName());
@@ -35,7 +40,38 @@ public class DirectoryInfo {
             }
 
             return output.toString();
+    }
+
+    /**
+     *
+     * @param subDir List of subDirectories names
+     * @return Requested Directory by absolute Path
+     * Takes in a List of subDirectories names, returns the last Folder in the provided Path
+     */
+    public static BaseFileSystemObject getFileSystemByAbsolutPath(List<String> subDir, Host host) throws FileNotExistsException,
+            NotADirectoryException {
+        if(subDir.get(0).isEmpty()) subDir.remove(0);
+        BaseFileSystemObject directory = host.getRootFileSystem();
+        try {
+            for (String subDirectory: subDir) {
+                directory = directory.getSpecificChildren(subDirectory);
+                if(!(directory instanceof DirectoryObject)){
+                    throw new NotADirectoryException(directory.getName());
+                }
+            }
+            return directory;
         }
+        catch (NullPointerException e)
+        {
+            throw new FileNotExistsException();
+        }
+
+    }
+
+    public static List<String> pathToArray(String path){
+        String[] subDirectories = path.split("/");
+        return new ArrayList<>(Arrays.asList(subDirectories));
+    }
 
 }
 
