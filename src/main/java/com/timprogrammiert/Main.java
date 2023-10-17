48b095703c5d324c056605e22f78e0035b15c7e5
Tim <tmatzenauer@hotmail.de>
2023-10-17 11:12:40 +0200

    Deleted unused imports

--- src/main/java/com/timprogrammiert/Main.java
+++ src/main/java/com/timprogrammiert/Main.java
@@ -1,9 +1,6 @@
 package com.timprogrammiert;
 
-import com.timprogrammiert.filesystem.DirectoryObject;
-import com.timprogrammiert.host.Host;
 import com.timprogrammiert.terminal.Terminal;
-import com.timprogrammiert.util.DirectoryInfo;
 
 public class Main {
     public static void main(String[] args) {
