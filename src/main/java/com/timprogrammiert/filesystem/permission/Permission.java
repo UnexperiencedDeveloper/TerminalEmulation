3267676f76c910f2e282761a3592b1ad30baf52d
UnexperiencedDeveloper <47007198+UnexperiencedDeveloper@users.noreply.github.com>
2023-10-17 19:46:03 +0200

    Now needs on initialisation a permissionString like in linux

--- src/main/java/com/timprogrammiert/filesystem/permission/Permission.java
+++ src/main/java/com/timprogrammiert/filesystem/permission/Permission.java
@@ -1,26 +1,43 @@
 package com.timprogrammiert.filesystem.permission;
 
-public class Permission {
-    private String userPermission;
-    private String groupPermission;
-    private String otherPermission;
+import com.timprogrammiert.filesystem.util.PermissionCreater;
 
-    public String getUserPermission(){
-        return userPermission;
+/**
+ * Represent the Persmissions for each File / Directory
+ */
+public class Permission {
+    public static String DEFAULT_FILE_PERMISSION = "rw-rw-rw-";
+    public static String DEFAULT_DIRECTORY_PERMISSION = "rwxrwxrwx";
+    private String permissionString;
+    private User user;
+    private UserGroup userGroup;
+
+    /**
+     * Creates a new Permission Object, can be attached to a File / Directory Object
+     * Permissions are represented like in linux. e.g. only user can read and write = rw-------
+     * Directories must have the ability to execute, otherwise user is not allowed to cd in Directory
+     * @param user The User who owns this permission
+     * @param permissionString The permission String, can be defaulted via PermissionCreater
+     */
+    public Permission(User user, String permissionString){
+        this.user = user;
+        this.permissionString = permissionString;
+        userGroup = user.getUserGroups().get(0); // Get Default Group, as this is the first one set
     }
-    public String getGroupPermission(){
-        return groupPermission;
+
+    public String getPermissionString() {
+        return permissionString;
     }
-    public String getOtherPermission(){
-        return otherPermission;
+
+    public void setPermissionString(String permissionString) {
+        this.permissionString = permissionString;
     }
-    public void setUserPermission(String userPermission){
-        this.userPermission = userPermission;
+
+    public User getUser() {
+        return user;
     }
-    public void setGroupPermission(String groupPermission){
-        this.groupPermission = groupPermission;
-    }
-    public void setOtherPermission(String otherPermission){
-        this.otherPermission = otherPermission;
+
+    public UserGroup getUserGroup() {
+        return userGroup;
     }
 }
