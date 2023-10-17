262c7126c1ed7f9f1ea7661e7a5a4c67f1df0194
Tim <tmatzenauer@hotmail.de>
2023-10-17 09:23:47 +0200

    Small Rework of DirectoryInfo Logic - Usage of Generics - Search the Specified File Type - No longer throws NotADirectory, should be thrown by the calling Method

--- src/main/java/com/timprogrammiert/util/DirectoryInfo.java
+++ src/main/java/com/timprogrammiert/util/DirectoryInfo.java
@@ -43,29 +43,31 @@ public class DirectoryInfo {
     }
 
     /**
-     * Each SubPath is resolved to its BaseFileSystemObject, and its children and so on.
-     * If the provided Children Path Contains a FileObject which is not the target of the path, Exception
-     * will be thrown
-     * @param subDir List of subDirectories names like ['path', 'to', 'file' ]
-     * @return Requested Directory by absolute Path
-     * Takes in a List of subDirectories names, returns the last Folder in the provided Path
+     * The Absolut Path is splitted into Arrays (use DirectoryInfo.PathToArray) and parsed until the last element of
+     * the array, which represents the Target rather it be a File or Directory it gets Returned.
+     * Specifiy the Object you want to get as childType
+     * If the Object is not found a null Object is returned
+     * @param subDir AbsolutPath splitted via DirectoryInfo.PathToArray
+     * @param host The host the filesystem belongs to
+     * @param childType DirectoryObject.class or FileObject.class
+     * @return If the found Object is an Instance of the given Class then it will be returned, otherwise
+     * an Excpetion ClassCastException will be thrown
+     * @param <T> DirecotyObject.class or FileObject.class
+     * @throws FileNotExistsException Thrown by BaseFileSystemObject when the Object is null
+     * @throws ClassCastException If the found Object is not an Instance of the given Class
      */
-    public static BaseFileSystemObject getFileSystemByAbsolutPath(List<String> subDir, Host host) throws FileNotExistsException,
-            NotADirectoryException {
+    public static <T extends BaseFileSystemObject> T getFileSystemByAbsolutPath(List<String> subDir, Host host, Class<T> childType) throws
+            ClassCastException, FileNotExistsException {
         if(subDir.get(0).isEmpty()) subDir.remove(0);
-        BaseFileSystemObject directory = host.getRootFileSystem();
-        try {
-            for (String subDirectory: subDir) {
-                directory = directory.getSpecificChildren(subDirectory);
-                if(!(directory instanceof DirectoryObject)){
-                    throw new NotADirectoryException(directory.getName());
-                }
-            }
-            return directory;
+        BaseFileSystemObject objectToSearch = host.getRootFileSystem();
+
+        for (String subDirectory: subDir) {
+            objectToSearch = objectToSearch.getSpecificChildren(subDirectory);
         }
-        catch (NullPointerException e)
-        {
-            throw new FileNotExistsException();
+        if(childType.isInstance(objectToSearch)){
+            return childType.cast(objectToSearch);
+        }else{
+            throw new ClassCastException();
         }
 
     }
@@ -75,34 +77,48 @@ public class DirectoryInfo {
         return new ArrayList<>(Arrays.asList(subDirectories));
     }
 
+
     /**
-     * Used to get the File / Directory with a Single Relative Path like
-     * e.g. current Path is /etc, requested File is /etc/FileName
-     * @param childrenName the Children File / Directory requested
-     * @param host The host the children belongs to
-     * @return The requested File / Directory, Returns NULL if the requested File / Directory does not exist
-     * @throws FileNotExistsException
+     * Search for the specified children in currentDirectory (gained by host)
+     * @param childrenName The name of the Children you want to recieve
+     * @param host The host the Filesystem belongs to
+     * @param childType The Type the Children belongs to (DirectoryObject, FileObject)
+     * @return The Children, if its an Instance of specified Class
+     * @throws FileNotExistsException Is thrown by BaseFileSystemObject when the Object is Null
+     * @throws ClassCastException Is thrown when the Specified Class does not match with the Found Object
      */
-    public static BaseFileSystemObject resolveSingleRelativePath(String childrenName, Host host) throws FileNotExistsException {
-        return host.getCurrentDirectory().getSpecificChildren(childrenName);
+    public static <T extends BaseFileSystemObject> T  resolveSingleRelativePath(String childrenName, Host host, Class<T> childType)
+            throws FileNotExistsException, ClassCastException {
+
+        if(childType.isInstance(host.getCurrentDirectory().getSpecificChildren(childrenName))){
+            return childType.cast(host.getCurrentDirectory().getSpecificChildren(childrenName));
+        }else {
+            throw new ClassCastException();
+        }
     }
 
     /**
-     * Used to get the File / Directory with a Multi Relative Path like
-     * e.g. current Path is /etc, requested File is /etc/anotherDirectory/FileName
-     * @param argList Relative Path as a List
-     * @param host
-     * @return The Requested File / Directory
-     * @throws FileNotExistsException
+     * The PathString gets converted into an Array, splitted by "/" as SubDirectory
+     * Each subDirectory is searched in the Children List of the current looped Directory
+     * @param pathString the relative Path to the Object (e.g path/to/file)
+     * @param host The Host the Filesystem belongs to
+     * @param childType The Type the Children belongs to (DirectoryObject, FileObject)
+     * @return The Children, if its an Instance of the specified Class
+     * @throws FileNotExistsException Is thrown by BaseFileSystemObject when the Object is Null
+     * @throws ClassCastException Is thrown when the Specified Class does not match with the Found Object
      */
-    public static BaseFileSystemObject resolveMultiRelativePath(List<String> argList, Host host) throws FileNotExistsException {
-        String[] subDirectoriesStrings = argList.get(0).split("/");
+    public static <T extends BaseFileSystemObject> T resolveMultiRelativePath(String pathString, Host host, Class<T> childType)
+            throws FileNotExistsException, ClassCastException {
+        String[] subDirectoriesStrings = pathString.split("/");
         BaseFileSystemObject recursiveDir = host.getCurrentDirectory();
         for (String dirName: subDirectoriesStrings) {
             recursiveDir = recursiveDir.getSpecificChildren(dirName);
-            if(recursiveDir == null) throw new FileNotExistsException();
         }
-        return recursiveDir;
+        if(childType.isInstance(recursiveDir)){
+            return childType.cast(recursiveDir);
+        }else {
+          throw new ClassCastException();
+        }
     }
 
 }
